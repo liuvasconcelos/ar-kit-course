cc94d431e5b226b15b0730cdcd8725354a1f5c1b
liuvasconcelos <livia.vasconcelos@stant.com.br>
2019-12-08 11:55:41 -0300

    develop Update add method to put spheres in random positions.
    -
    
    Signed-off-by: liuvasconcelos <livia.vasconcelos@stant.com.br>

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -22,16 +22,23 @@ class ViewController: UIViewController {
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,
                                        ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
     }
 
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
         // shape of node, in this case is a box.
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.1/2)
         // firstMaterial is the texture of the geometry and diffuse is the color
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        //specular is the light that the object reflects.
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        
+        let x = randomNumbers(firstNumber: -0.3, secondNumber: 0.3)
+        let y = randomNumbers(firstNumber: -0.3, secondNumber: 0.3)
+        let z = randomNumbers(firstNumber: -0.3, secondNumber: 0.3)
         //Vector3 -> 3 dimensions
-        node.position = SCNVector3(0,0,-0.3)
+        node.position = SCNVector3(x,y,z)
         
         //Root node has no attributes (color, size) and is positioned where camera starts.
         //When add a childnode, this child is always relative to the rootnode.
@@ -50,6 +57,11 @@ class ViewController: UIViewController {
         
         // .resetTracking removes the previous coordinators and create a new one based on the position that you are.
         self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors] )
+    }
+
+    func randomNumbers(firstNumber: CGFloat, secondNumber: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNumber - secondNumber)
+            + min(firstNumber, secondNumber)
     }
 }
 
